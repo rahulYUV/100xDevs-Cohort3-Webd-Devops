9c8dcb5fe234d3d8286a37a67c858700af4c88d3
rahulYUV <rs6205903996@gmail.com>
2025-07-20 02:24:21 +0530

    sheriyans js Foundation

--- Week 01 - Orientation, HTML,CSS,Basic JS/sheriyans/namaste/script.js
+++ Week 01 - Orientation, HTML,CSS,Basic JS/sheriyans/namaste/script.js
@@ -1,307 +1,361 @@
-// var a =10;
-// var a =30;
-// console.log(a);
-const student ={name:"rahul kumar"};
-student.name="kirat";
-console.log(student.name);// const obj allow modification 
+// ============================================
+// JAVASCRIPT FUNDAMENTALS PRACTICE
+// ============================================
 
-// {
-//     let x =10;
-//     var y =23;
-//     let y =34;
-//     // console.log(y); syntax error
-// }
+// ============================================
+// 1. VARIABLES AND SCOPE
+// ============================================
 
-// console.log(y);
-// console.log(x)// reference error
-var a =12;
-// window mein add karta hai 
-// it is function scope
+// Variable declarations and modifications
+const student = {name: "rahul kumar"};
+student.name = "kirat";
+console.log(student.name); // const objects allow property modification
 
-//const protect the variable 
-const discount =12;
+// Scope examples
+var a = 12; // Function scoped, added to window object
+const discount = 12; // Block scoped, cannot be reassigned
+let z = 12; // Block scoped, can be reassigned
 
-// temporal DEAD Zone 
-// console.log(z);
+const name = "rahul";
+const city = "pune";
+let age = 19;
 
-let z =12;
+// ============================================
+// 2. DATA TYPES
+// ============================================
 
-const name="rahul";
-const city="pube";
-let age =19;
-
-
-// primitives  
-//number , float , boolean , null , undefined, symbol,biginit
-
-// referece types , non primitives 
-// arr , obj , function 
-
-// let a = 12;// primitve // number 
-let String = "rahul kumar"
+// Primitive types: number, string, boolean, null, undefined, symbol, bigint
+let String = "rahul kumar";
 let flag = true;
-let arr=[1,2,3,4,5];
-let brr = arr;
+let arr = [1, 2, 3, 4, 5];
+
+// Reference types behavior
+let brr = arr; // Both variables point to the same array
 brr.pop();
 brr.pop();
 arr.push(5);
-// console.log(arr);
-// console.log(arr);
 
-// '' singlw quote 
-// " backticks "=? ``
-// double ticks =""
-
-let std =null;
-std =+5;
-// console.log(std);?//
-// symbol -> unique immutable vaue;
+// Symbol - unique immutable values
 let u1 = Symbol("uid");
 let u2 = Symbol("uid");
-// they are different 
+// u1 and u2 are different even with same description
 
-// bigInt
-// very large number 
+// Null and type conversion
+let std = null;
+std = +5; 
 
+// ============================================
+// 3. FUNCTIONS
+// ============================================
 
-// reference type{bracket dikhe()};
-
-let ab = [1,2,3];
-let user ={};
-// let userr=(a);
-// console.log(typeof(a));
-
-// early return value pattern 
-
-
-// early return value ;
-
-function getval(){
-    if(num ==2){
-        return num;
+// Function to calculate grades based on marks
+function getGrade(marks) {
+    if (marks >= 75 && marks <= 100) {
+        return "distinction";
+    } else if (marks >= 50 && marks < 75) {
+        return "first class";
+    } else {
+        return "below 50 percent";
     }
 }
-function fetGrade(marks){
-    if(marks >=75 && marks<=100){
-        return `distinction`
-    }else if(marks>=50 && marks<75){
-        return `first class`;
 
-    }
-    else{
-        return ` below 50 percent`;
-    }
-}
-// console.log("grade start from here");
-// console.log(fetGrade(75));// distincation 
-// console.log(fetGrade(76));// distincation 
-// console.log(fetGrade(55));// first class
-// console.log(fetGrade(46));// below
-// console.log(fetGrade(10));// below
+// Example usage:
+// console.log(getGrade(75)); // distinction
+// console.log(getGrade(55)); // first class
+// console.log(getGrade(46)); // below 50 percent
 
-
-// let i =0;
-// 
-// for(let i =1;i<=10;i++){
-//     console.log(i);
-// }
-// let zz =1;
-// while(zz!=10){
-//     console.log(zz);
-//     zz++;
-// }
-
-let sum =0;
-for(let i=1;i<=100;i++) {
-    sum += i;
-
-
-}
-
-//   let value = prompt("enter a number "); 
-//   console.log(value);     
-
-// console.log(sum);
-// console.log("this is clear");
-// divisible by 3 
-// for(let k = 1;k<=50;k++){
-//     if(k%3==0){
-//         console.log(k);
-//     }
-// }
-// reversh a string 
-
-
-// fucntion 
-
-function calm1(){
+// Function declarations
+function calm1() {
     console.log("keep calm");
     console.log("keep calm");
     console.log("keep calm");
 }
 
-calm1();
-
-let ask = function(){
+// Function expressions
+let ask = function() {
     console.log("we do");
+};
+
+// Arrow functions
+let arrow = () => {
+    console.log("hurra");
+};
+
+// ============================================
+// 4. ADVANCED FUNCTION CONCEPTS
+// ============================================
+
+// Rest parameters - collect multiple arguments into an array
+function restExample(...values) {
+    console.log(values);
 }
+restExample(1, 2, 3, 45, 6, 7, 8);
 
-ask(); // we do
-// console.log(ask);// [Function: ask]
-
-// fn dec 
-// fn name (){};// fmc expresion 
-
-// ()=>{};
-// let varr = ()=>{console.log("hurra")};
-// varr();
-
-// rest amd spread;
-
-function restt(... val){
-    console.log(val);
-
-}
-restt(1,2,3,45,6,7,8);
-// ... if its appleid in fucntiaon parameter then its known as rest 
-// / ... attach with fucnation and object then its know as  spread
-// first class function 
-// exist in js , thosse funcation which can be treate as a value 
-// funcation that can treated as a value 
-let abcd = function(){
-
-}
-
-// first class funcation 
-function shout(msg){
+// First-class functions - functions treated as values
+function shout(msg) {
     return msg.toUpperCase();
-
 }
-function processMessage(fn){
+
+function processMessage(fn) {
     console.log(fn("hello"));
 }
-processMessage(shout)
+processMessage(shout);
 
- // a high order function is a function that accept a funcation as a argument or return a function 
-
-// high order function 
-// those function which can return or except a fucnation in her parameter 
-// pure function 
-// a funcation which can not change the outside value
-// a function which can change the outsider value 
-// known as impure function 
-// clousers = a function who return a fun and the return func who use parent 
-
-function creteMul(x){
-    return function(y){
-        return x*y;
-    }
-}
-let double = creteMul(2);
-console.log(double(2));
-
-console.log(double(3));
-
-
-// closure 
-// a function that return a function and user a vairrable of parents 
-
-function createCounter(){
-    let count =0;
-    return function(){
-        // this is the inner function (the clouser)
-        count+=1;
-        return count ;
-    }
+// Higher-order function - accepts or returns functions
+function createMultiplier(x) {
+    return function(y) {
+        return x * y;
+    };
 }
 
-const counter1 = createCounter(); // counter one is a clouser
-console.log(counter1());
-console.log(counter1());
+let double = createMultiplier(2);
+console.log(double(2)); // 4
+console.log(double(3)); // 6
 
+// ============================================
+// 5. CLOSURES
+// ============================================
+
+// Closure - function that returns a function and uses parent's variables
+function createCounter() {
+    let count = 0;
+    return function() {
+        // This is the inner function (the closure)
+        count += 1;
+        return count;
+    };
+}
+
+const counter1 = createCounter(); // counter1 is a closure
+console.log(counter1()); // 1
+console.log(counter1()); // 2
 
 const counter2 = createCounter();
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
+console.log(counter2()); // 1
+console.log(counter2()); // 2
+console.log(counter2()); // 3
 
+// ============================================
+// 6. IMMEDIATELY INVOKED FUNCTION EXPRESSIONS (IIFE)
+// ============================================
 
-// iife 10 time 
-
-(
-    function(){
-
-    }
-)();
-(
-    function(){
-
-    }
-)();
-(function play(){
-
+// IIFE - Functions that execute immediately
+(function() {
+    // This function runs immediately
 })();
 
-(
-    function(){
-        // this is function body;
-    }
-)();
+(function() {
+    // Another IIFE example
+})();
 
-(
-    function(){
-        // this is funcation body as well 
-    }
-)();// this is iife
+(function play() {
+    // Named IIFE
+})();
 
-(
-    function(){
-        // this is function body
-    }
-)();
+// ============================================
+// 7. ARROW FUNCTIONS AND DEFAULT PARAMETERS
+// ============================================
 
-
-// (
-//     function hurra(){
-//             console.log("hurra");
-//     }
-// )();
-console.log(" we are using by its name")
-
-//hurra();// im using this out side of its scope 
-
-function mull(num1,num2){
-    return num1*num2;
-
+// Converting regular function to arrow function
+function multiply(num1, num2) {
+    return num1 * num2;
 }
-// convert this inot arrow function
- let ans = (a,b)=>{
-    return a*b;
+
+// Arrow function equivalent
+let multiplyArrow = (a, b) => {
+    return a * b;
+};
+
+// Default parameters
+function sayHi(name = "guest") {
+    console.log("Hi", name);
+}
+// sayHi(); // Output: Hi guest
+
+// Rest parameters - multiple arguments as array
+function calculateSum(...scores) {
+    let sum = 0;
+    for (let i = 0; i < scores.length; i++) {
+        sum += scores[i];
+    }
+    return sum;
+}
+// console.log(calculateSum(21, 3, 4, 6, 7));
+
+
+// ============================================
+// 8. PRACTICAL EXAMPLES
+// ============================================
+
+// BMI Calculator
+function calculateBMI(weight, height) {
+    return weight / (height * height);
+}
+console.log(calculateBMI(54, 5.4).toFixed(2));
+
+// Discount Calculator using Higher-Order Functions
+function discountCalculator(discount) {
+    return function(price) {
+        return price - (price * discount / 100);
+    };
+}
+
+// ============================================
+// 9. ARRAY OPERATIONS
+// ============================================
+
+// Basic array operations
+let marks = [0, 1, 2, 3, 4, 5, 6];
+console.log(marks[3]); // Access element at index 3
+
+// Array modification methods
+marks.pop(); // Remove last element
+marks.splice(2, 2); // Remove 2 elements starting from index 2
+let slicedArray = marks.slice(1, 2); // Extract elements without modifying original
+
+// Array iteration with forEach
+let numbers = [1, 2, 3, 34, 4, 5];
+numbers.forEach(function(value) {
+    console.log(value);
+});
+
+// Arrow function with forEach
+numbers.forEach(element => {
+    console.log(element);
+});
+
+// ============================================
+// 10. ARRAY METHODS (MAP, FILTER, REDUCE)
+// ============================================
+
+// Map - transform each element
+let numbersArray = [1, 2, 3, 4, 45];
+let incrementedNumbers = numbersArray.map(function(val) {
+    return val + 1;
+});
+console.log(incrementedNumbers);
+
+// Filter - select elements based on condition
+let evenNumbers = numbersArray.filter((e) => {
+    return e % 2 === 0;
+});
+
+// Reduce - combine all elements into single value
+let arrayForReduce = [1, 2, 3, 4, 5, 6];
+const sum = arrayForReduce.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;
+}, 0);
+console.log(sum);
+
+// ============================================
+// 11. SPREAD OPERATOR AND ARRAY MANIPULATION
+// ============================================
+
+// Spread operator - create copy of array
+let originalArray = [1, 2, 3, 4, 5, 6];
+let copiedArray = [...originalArray];
+copiedArray.pop();
+console.log(copiedArray); // Modified copy
+console.log(originalArray); // Original unchanged
+
+// Array methods for adding/removing elements
+let fruits = ["apple", "banana", "litchi"];
+console.log(fruits[1]); // Access element
+fruits.unshift("mango"); // Add to beginning
+fruits.push("pineapple"); // Add to end
+console.log(fruits);
+
+// ============================================
+// 12. ARRAY SORTING
+// ============================================
+
+// Note: Fixed variable name issue - using 'sortArray' instead of undefined 'arr'
+let sortArray = [10, 20, 30];
+sortArray.sort((a, b) => {
+    return b - a; // Sort in descending order
+});
+console.log(sortArray);
+
+
+
+
+
+// object 
+ const user ={
+    name:"rahul",
+    address:{
+        city:"patna",
+        location:{
+            lat:2314,
+            lng:77.3
+        }
+    }
  }
-//  console.log(ans(5,6));
+// print lat
+console.log(user.address.location.lat)
 
-function sayHI(name="guest"){
-    console.log("hi",name);
+let userr ={
+    name:"rahul",
+    age:24,
+    email:"test@test.com"
+}
+
+for(let key in userr){
+    console.log(key);
+    console.log(userr[key]);
+
+
+    
+}
+let obj={
+    name:"this is obj1",
+    isLoggesIn:true
+}
+// console.log(obj);
+// const obj2 ={... obj};
+// console.log(obj2);
+
+// deep clone 
+// we convert in stringfy
+// JSON.stringify()
+
+// JSON.stringify(obj)
+
+// let obj3 = JSON.parse(JSON.stringify(obj))
+// // console.log(obj3);
+// // optinal chaining
+
+
+let objj={
+    name:"this is obj1",
+    isLoggesIn:true
+}
+
+// console.log(objj?.nammee);
+
+let studentt ={
+     name:"rahul",
+    age:19,
+    isEnrolled:true
+}
+
+let key ="admin"
+const r ={
+    admin:"Rahul "
 
 }
-// sayHI();// hi guest // defualt value 
-//... mean it can take multiple argument => rest operatore 
-// and the arguemnt is the array
+console.log(r[key]);
+let{ "admin":adminn }= r;
 
-function takeEasy(... score){
-    let sum =0;
+console.log(adminn);
 
-    for(let i =0;i<score.length;i++){
-        console.log(score[i]);
-       sum += score[i];
-
-
-    }
-    return sum ;
-
+for(let key in r){
+    console.log(key);
 }
-// console.log("all clear");
-//  console.log(takeEasy(21,3,4,6,7));
+const courses ={
+    title:"js",
+    duration:"4 weeks"
+}
+for(let key in courses){
+    console.log(key +" is "+ courses[key]);
+}
